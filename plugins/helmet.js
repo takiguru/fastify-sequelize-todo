eda66d93ef5e7664c1dd838df852395f0d2b8e75
Peter Takacs <takpecc@gmail.com>
2021-02-07 17:34:39 +0100

    fix(swagger): exclude swagger from helmet

--- plugins/helmet.js
+++ plugins/helmet.js
@@ -1,6 +1,25 @@
 const internals = require('fastify-helmet');
 const config = require('config');
 const helmetConfig = config.get('helmet');
-const options = helmetConfig;
 
-module.exports = { internals, options };
+const onRegister = (instance) => {
+    return Object.assign(
+        {
+            //Fix for swagger with helmet
+            contentSecurityPolicy: {
+                directives: {
+                    ...internals.contentSecurityPolicy.getDefaultDirectives(),
+                    'form-action': ["'self'"],
+                    'img-src': ["'self'", 'data:', 'validator.swagger.io'],
+                    'script-src': ["'self'"].concat(instance.swaggerCSP.script),
+                    'style-src': ["'self'", 'https:'].concat(
+                        instance.swaggerCSP.style,
+                    ),
+                },
+            },
+        },
+        helmetConfig,
+    );
+};
+
+module.exports = { internals, onRegister };
--- plugins/index.js
+++ plugins/index.js
@@ -3,7 +3,7 @@ const helmetPlugin = require('./helmet');
 
 const register = (fastify) => {
     fastify.register(swaggerPlugin.internals, swaggerPlugin.options);
-    fastify.register(helmetPlugin.internals, helmetPlugin.options);
+    fastify.register(helmetPlugin.internals, helmetPlugin.onRegister);
 };
 
 module.exports = { register };
