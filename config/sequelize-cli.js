13a098137dd77c04260a0f4b52122a028d51e1fd
Peter Takacs <takpecc@gmail.com>
2021-02-08 01:19:48 +0100

    fix(database): incorrect env variable

--- config/sequelize-cli.js
+++ config/sequelize-cli.js
@@ -1,7 +1,7 @@
 const sequelizeCli = require('config');
 const sequelizeConfig = sequelizeCli.get('database.sequelizeOptions');
 
-const currentEnvironment = process.NODE_ENV || 'development';
+const currentEnvironment = process.env.NODE_ENV || 'development';
 
 const configuration = {
     [currentEnvironment]: sequelizeConfig,
